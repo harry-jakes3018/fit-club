ccdfc60c016cb789c30d3fb9788bb34530748b35
Jitendra Garg <107532341+harry-jakes3018@users.noreply.github.com>
2022-08-09 21:35:10 +0530

    Starter Files Added

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <span style={{ color: "white" }}>Jitendra Garg</span>
+        </div>
+    );
+}
+
+export default App;
