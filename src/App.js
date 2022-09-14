abf813a6283630e8807869af667f6665eb78e5bf
Jitendra Garg <107532341+harry-jakes3018@users.noreply.github.com>
2022-09-14 22:57:27 +0530

    Programs Component Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import HeroSection from "./components/Hero Section/HeroSection";
+import Programs from "./components/Programs/Programs";
 
 function App() {
     return (
         <div className="App">
             <HeroSection />
+            <Programs />
         </div>
     );
 }
