8e59d14923f511e348a71d2fed68242bac0bdf2f
Jitendra Garg <107532341+harry-jakes3018@users.noreply.github.com>
2022-09-17 22:25:39 +0530

    Reasons Section Completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import HeroSection from "./components/Hero Section/HeroSection";
 import Programs from "./components/Programs/Programs";
+import Reasons from "./components/Reasons/Reasons";
 
 function App() {
     return (
         <div className="App">
             <HeroSection />
             <Programs />
+            <Reasons />
         </div>
     );
 }
