363f4e84cd8a21e275e3769b5b61b105c428823c
Jitendra Garg <107532341+harry-jakes3018@users.noreply.github.com>
2022-10-02 11:40:45 +0530

    Testimonials Section Completed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HeroSection from "./components/Hero Section/HeroSection";
 import Plans from "./components/Plans/Plans";
 import Programs from "./components/Programs/Programs";
 import Reasons from "./components/Reasons/Reasons";
+import Testimonials from "./Testimonials/Testimonials";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
             <Programs />
             <Reasons />
             <Plans />
+            <Testimonials />
         </div>
     );
 }
